1e2535c69aa57f26569b0629356e4c1f0324fb44
Hesham Mathana <heshammathana0@gmail.com>
2025-02-26 23:19:15 +0200

    Apply For Fluent Syntax  >> Extention <Method

--- Session01/Session01/Program.cs
+++ Session01/Session01/Program.cs
@@ -8,12 +8,17 @@
             //List<int> OddNums = list.Where(num => num%2==0).ToList();
             //foreach (int num in OddNums) { Console.WriteLine(num); }
 
-            List<int> list = new List<int>() { 1,2,3,4,5,6,7,8,9};
-            var OddNumber =Enumerable.Where(list,num => num % 2 == 0);
-            foreach (var num in OddNumber)
-            {
-                Console.Write($"{num} ");
-            }
+            //List<int> list = new List<int>() { 1,2,3,4,5,6,7,8,9};
+            //var OddNumber =Enumerable.Where(list,num => num % 2 == 0);
+            //foreach (var num in OddNumber)
+            //{
+            //    Console.Write($"{num} ");
+            //}
+
+            List<int> list = new List<int>() { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+            var OddNumber = list.Where(num => num % 2 == 0);
+            foreach (int num in OddNumber)  {Console.WriteLine(num); }
+
         }
     }
 }
