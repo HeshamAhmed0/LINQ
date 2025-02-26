702e465a65e2c2f599f0356e7d6b5ffc8136a997
Hesham Mathana <heshammathana0@gmail.com>
2025-02-26 22:16:37 +0200

    Apply For Extention Method

---
+++ Session01/Session01/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace Session01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> list = new List<int>() { 1,2,3,4,5,6};
+            List<int> OddNums = list.Where(num => num%2==0).ToList();
+            foreach (int num in OddNums) { Console.WriteLine(num); }
+        }
+    }
+}
