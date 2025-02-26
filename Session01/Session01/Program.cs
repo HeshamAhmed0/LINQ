e16f19cf520a96bbb67388b58ed2932304ef256a
Hesham Mathana <heshammathana0@gmail.com>
2025-02-27 00:27:58 +0200

    Apply For Extention ways

--- Session01/Session01/Program.cs
+++ Session01/Session01/Program.cs
@@ -24,11 +24,27 @@
             #endregion
 
             #region Query Syntex
-            List<int> list = new List<int>() { 1,2,3,4,5,6,7};
-            var OddNumbers = from L in list
-                             where L % 2 == 0
-                             select L;
-            foreach (var o in OddNumbers) { Console.Write($"{o} "); }
+            //List<int> list = new List<int>() { 1,2,3,4,5,6,7};
+            //var OddNumbers = from L in list
+            //                 where L % 2 == 0
+            //                 select L;
+            //foreach (var o in OddNumbers) { Console.Write($"{o} "); }
+            #endregion
+            #endregion
+
+            #region Execution Ways
+            #region Deferred Executio [Last Version Of Elements]
+            //List<int> numbers = new List<int>() { 1, 2, 3, 4, 5, 6, 7, 8 };
+            //var EvenNumbers = numbers.Where(num => num % 2 == 1);
+            //numbers.AddRange(new int[] { 1, 101, 20, 401, 50 });
+            //foreach (var Numbers in EvenNumbers) { Console.WriteLine($"{Numbers} "); }
+            #endregion
+
+            #region Immediate Execution
+            //List<int> numbers = new List<int>() { 1, 2, 3, 4, 5, 6, 7, 8 };
+            //var EvenNumbers = numbers.Where(num => num % 2 == 1).ToList();
+            //numbers.AddRange(new int[] { 1, 101, 20, 401, 50 });
+            //foreach (var Numbers in EvenNumbers) { Console.WriteLine($"{Numbers} "); }
             #endregion
             #endregion
 
