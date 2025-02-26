a9a39265e47dad7b2f7dca02193181ab1e65f98f
Hesham Mathana <heshammathana0@gmail.com>
2025-02-26 23:15:29 +0200

    Apply For Fluent Syntax  >> Static

--- Session01/Session01/Program.cs
+++ Session01/Session01/Program.cs
@@ -4,9 +4,16 @@
     {
         static void Main(string[] args)
         {
-            List<int> list = new List<int>() { 1,2,3,4,5,6};
-            List<int> OddNums = list.Where(num => num%2==0).ToList();
-            foreach (int num in OddNums) { Console.WriteLine(num); }
+            //List<int> list = new List<int>() { 1,2,3,4,5,6};
+            //List<int> OddNums = list.Where(num => num%2==0).ToList();
+            //foreach (int num in OddNums) { Console.WriteLine(num); }
+
+            List<int> list = new List<int>() { 1,2,3,4,5,6,7,8,9};
+            var OddNumber =Enumerable.Where(list,num => num % 2 == 0);
+            foreach (var num in OddNumber)
+            {
+                Console.Write($"{num} ");
+            }
         }
     }
 }
