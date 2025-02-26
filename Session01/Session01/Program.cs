e66011d46c4df51fad3386cd026408a1e3dcc980
Hesham Mathana <heshammathana0@gmail.com>
2025-02-26 23:37:11 +0200

    Apply For Query Syntax

--- Session01/Session01/Program.cs
+++ Session01/Session01/Program.cs
@@ -4,6 +4,9 @@
     {
         static void Main(string[] args)
         {
+            #region Linq Syntax
+
+            #region Fluent Syntex
             //List<int> list = new List<int>() { 1,2,3,4,5,6};
             //List<int> OddNums = list.Where(num => num%2==0).ToList();
             //foreach (int num in OddNums) { Console.WriteLine(num); }
@@ -15,9 +18,19 @@
             //    Console.Write($"{num} ");
             //}
 
-            List<int> list = new List<int>() { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
-            var OddNumber = list.Where(num => num % 2 == 0);
-            foreach (int num in OddNumber)  {Console.WriteLine(num); }
+            //List<int> list = new List<int>() { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+            //var OddNumber = list.Where(num => num % 2 == 0);
+            //foreach (int num in OddNumber)  {Console.WriteLine(num); }  
+            #endregion
+
+            #region Query Syntex
+            List<int> list = new List<int>() { 1,2,3,4,5,6,7};
+            var OddNumbers = from L in list
+                             where L % 2 == 0
+                             select L;
+            foreach (var o in OddNumbers) { Console.Write($"{o} "); }
+            #endregion
+            #endregion
 
         }
     }
